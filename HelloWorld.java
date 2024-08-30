a05894cab1174ecc990a0a9561284127defafc58
poojadahifale <100355419+poojadahifale@users.noreply.github.com>
2024-08-30 14:35:04 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
 static void method(){
 System.out.println("Hello java Method");
 System.out.println(" Hello changed java Method");
+  System.out.println("Hello");
 }
 
 public static void main(String[] args){
