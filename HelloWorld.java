716a1d1fa4bd27947992fa360900f64b7818b003
poojadahifale <100355419+poojadahifale@users.noreply.github.com>
2024-08-30 12:49:21 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+static void method(){
+System.out.println("Hello java Method");
+System.out.println(" Hello changed java Method");
+}
+
+public static void main(String[] args){
+System.out.println("Hello java World");
+method();
+}
